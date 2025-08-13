68ba672f01edba9ab48074e64b259bbfd2e7ece7
Neang Peou <neangpov66666@gmail.com>
2025-08-13 17:21:49 +0700

    commit

---
+++ frontend_admin/lib/controller/user_contoller.dart
@@ -0,0 +1,13 @@
+import 'package:get/get.dart';
+
+class UserController extends GetxController {
+  String userID = '';
+  String role = '';
+  String username = '';
+  String email = '';
+  String phoneNumber = '';
+  String passport = '';
+  String idCard = '';
+  String address = '';
+  String password = '';
+}
