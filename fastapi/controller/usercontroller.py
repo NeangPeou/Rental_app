9019c26e23aaeefca5008f3a43918e61ab3b27a2
Nim Rathana <126838139+NimRathana@users.noreply.github.com>
2025-08-18 12:09:58 +0700

    commit

--- fastapi/controller/usercontroller.py
+++ fastapi/controller/usercontroller.py
@@ -18,7 +18,7 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM], options={"verify_exp": False})
         username: str = payload.get("name")
         if username is None:
             raise credentials_exception
