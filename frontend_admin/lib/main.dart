f6e612dba8c1068ceea8faf9d83750e72983a314
Nim Rathana <nim.rathana11@gmail.com>
2025-08-26 11:12:51 +0700

    commit

--- frontend_admin/lib/main.dart
+++ frontend_admin/lib/main.dart
@@ -6,6 +6,7 @@ import 'package:frontend_admin/screens/authenticate/login.dart';
 import 'package:frontend_admin/screens/bottomNavigationBar/setting.dart';
 import 'package:frontend_admin/screens/home/home.dart';
 import 'package:frontend_admin/screens/wrapper.dart';
+import 'package:frontend_admin/services/inactivityService.dart';
 import 'package:frontend_admin/shared/constants.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
@@ -21,6 +22,7 @@ void main() async {
   await GetStorage.init();
   Get.put(SettingController());
   Get.put(UserController());
+  InactivityService().initialize();
   runApp(MyApp());
 }
 
@@ -36,20 +38,24 @@ class MyApp extends StatelessWidget {
       final selectedLang = _settingController.selectedLanguage.value;
       final selectedLocale = Setting.languages.firstWhere((lang) => lang['name'] == selectedLang, orElse: () => {'locale': Locale('en', 'US')})['locale'] as Locale;
 
-      return GetMaterialApp(
-        debugShowCheckedModeBanner: false,
-        translations: AppTranslations(),
-        locale: selectedLocale,
-        fallbackLocale: const Locale('en', 'US'),
-        initialRoute: '/',
-        themeMode: isDark ? ThemeMode.dark : ThemeMode.light,
-        theme: lightTheme(selectedColor),
-        darkTheme: darkTheme(selectedColor),
-        getPages: [
-          GetPage(name: '/', page: () => const Wrapper()),
-          GetPage(name: '/login', page: () => const Login()),
-          GetPage(name: '/home', page: () => const Home()),
-        ],
+      return Listener(
+        behavior: HitTestBehavior.translucent,
+        onPointerDown: (_) => InactivityService().userInteractionDetected(),
+        child: GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          translations: AppTranslations(),
+          locale: selectedLocale,
+          fallbackLocale: const Locale('en', 'US'),
+          initialRoute: '/',
+          themeMode: isDark ? ThemeMode.dark : ThemeMode.light,
+          theme: lightTheme(selectedColor),
+          darkTheme: darkTheme(selectedColor),
+          getPages: [
+            GetPage(name: '/', page: () => const Wrapper()),
+            GetPage(name: '/login', page: () => const Login()),
+            GetPage(name: '/home', page: () => const Home()),
+          ],
+        ),
       );
     });
   }
